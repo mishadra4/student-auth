4240517b616c8587f643071e05fef70cf79a99f4
Mykhailo Drach <mishadra4@gmail.com>
2020-05-19 18:54:56 +0300

    fixed qr code generation

--- src/main/java/com/md/sa/service/impl/QrCodeServiceImpl.java
+++ src/main/java/com/md/sa/service/impl/QrCodeServiceImpl.java
@@ -12,8 +12,6 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.io.IOException;
-import java.net.InetAddress;
 import java.time.LocalDateTime;
 
 import static org.springframework.util.StringUtils.isEmpty;
@@ -48,37 +46,27 @@ public class QrCodeServiceImpl implements QrCodeService {
 
     private QrCode createQrCode(Lecture lecture) {
         if (lecture.getQrCode() == null || isEmpty(lecture.getQrCode().getQrCodePath())) {
-            try {
-                InetAddress inetAddress = InetAddress.getLocalHost();
-                final String lectureUrl = "http://" + inetAddress.getHostAddress() + ":4200/student/lecture/" + lecture.getLectureId() + "/enroll";
-                final QrCode qrCode = new QrCode();
-                qrCode.setQrCodePath(lectureUrl);
-                qrCode.setEndDate(LocalDateTime.now().plusMinutes(activeMinutes));
-                entityManager.merge(qrCode);
-                lecture.setQrCode(qrCode);
-                entityManager.merge(lecture);
-                return qrCode;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            final String lectureUrl = "student/lecture/" + lecture.getLectureId() + "/enroll";
+            final QrCode qrCode = new QrCode();
+            qrCode.setQrCodePath(lectureUrl);
+            qrCode.setEndDate(LocalDateTime.now().plusMinutes(activeMinutes));
+            entityManager.merge(qrCode);
+            lecture.setQrCode(qrCode);
+            entityManager.merge(lecture);
+            return qrCode;
         }
         return lecture.getQrCode();
     }
 
     private QrCode createQrCode(Lab lab) {
         if (lab.getQrCode() == null || isEmpty(lab.getQrCode().getQrCodePath())) {
-            try {
-                InetAddress inetAddress = InetAddress.getLocalHost();
-                final String lectureUrl = "http://" + inetAddress.getHostAddress() + ":4200/student/lecture/" + lab.getLabId() + "/enroll";
-                final QrCode qrCode = new QrCode();
-                qrCode.setQrCodePath(lectureUrl);
-                entityManager.merge(qrCode);
-                lab.setQrCode(qrCode);
-                entityManager.merge(lab);
-                return qrCode;
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            final String lectureUrl = "student/lab/" + lab.getLabId() + "/enroll";
+            final QrCode qrCode = new QrCode();
+            qrCode.setQrCodePath(lectureUrl);
+            entityManager.merge(qrCode);
+            lab.setQrCode(qrCode);
+            entityManager.merge(lab);
+            return qrCode;
         }
         return lab.getQrCode();
     }
