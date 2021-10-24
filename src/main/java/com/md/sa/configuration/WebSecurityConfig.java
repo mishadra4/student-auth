1153aff5b096e4dc37efe6ffa441dd467a422dfd
Petro Shtenovych <petro_shtenovych@epam.com>
2021-10-24 16:05:56 +0300

    [feature] Ability to download lecture attachments

--- src/main/java/com/md/sa/configuration/WebSecurityConfig.java
+++ src/main/java/com/md/sa/configuration/WebSecurityConfig.java
@@ -59,7 +59,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable()
-                .authorizeRequests().antMatchers("/api/authenticate")
+                .authorizeRequests().antMatchers("/api/authenticate", "*/attachments/*")
                 .permitAll()
                 .anyRequest().authenticated()
                 .and()
