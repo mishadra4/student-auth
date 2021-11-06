f6a6ad20217de6dbce9793fe62da602167df8904
Petro Shtenovych <petro_shtenovych@epam.com>
2021-11-06 17:12:31 +0200

    [feature] Ability to notify students by absence for absence

--- src/main/java/com/md/sa/job/StudentGapNotifier.java
+++ src/main/java/com/md/sa/job/StudentGapNotifier.java
@@ -4,18 +4,21 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.md.sa.model.Subject;
 import com.md.sa.model.User;
 import com.md.sa.repository.StudentRepository;
 import com.md.sa.repository.SubjectRepository;
 import com.md.sa.service.EmailService;
-import org.quartz.Job;
-import org.quartz.JobExecutionContext;
 
 import static java.lang.String.format;
 
-public class StudentGapNotifier implements Job {
+@Component
+public class StudentGapNotifier {
+
+    private static final Integer ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
 
     private static final String MESSAGE = "Ти був відсутній на таких лекціях більше трьох разів: %s , звʼяжись з викладачем, щоб тебе не відрахували";
     private static final String SUBJECT = "Нагадування про погане відвідування";
@@ -26,8 +29,8 @@ public class StudentGapNotifier implements Job {
     @Autowired
     private SubjectRepository subjectRepository;
 
-    @Override
-    public void execute(JobExecutionContext jobExecutionContext) {
+    @Scheduled(fixedRate = ONE_WEEK)
+    public void notifyStudents() {
         studentRepository.findAll().stream()
             .map(User::getUsername)
             .forEach(this::sendNotification);
